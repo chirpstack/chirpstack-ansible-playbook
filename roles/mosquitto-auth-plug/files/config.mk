196672f52ec071b8b94c499d48b7c449d2ef9622
Orne Brocaar <info@brocaar.com>
2017-06-01 15:37:32 +0200

    Implement mosquitto-auth-plug integration.

---
+++ roles/mosquitto-auth-plug/files/config.mk
@@ -0,0 +1,24 @@
+# Select your backends from this list
+BACKEND_CDB ?= no
+BACKEND_MYSQL ?= no
+BACKEND_SQLITE ?= no
+BACKEND_REDIS ?= no
+BACKEND_POSTGRES ?= yes
+BACKEND_LDAP ?= no
+BACKEND_HTTP ?= no
+BACKEND_JWT ?= no
+BACKEND_MONGO ?= no
+BACKEND_FILES ?= yes
+
+# Specify the path to the Mosquitto sources here
+MOSQUITTO_SRC =
+
+# Specify the path the OpenSSL here
+OPENSSLDIR = /usr
+
+# Specify optional/additional linker/compiler flags here
+# On macOS, add 
+#	CFG_LDFLAGS = -undefined dynamic_lookup
+# as described in https://github.com/eclipse/mosquitto/issues/244
+CFG_LDFLAGS =
+CFG_CFLAGS = -DRAW_SALT
